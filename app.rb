fde96af4dfdfe6309b36ff7c4e1b8ce0121866bf
Alex Boyd <alex@opengroove.org>
2017-04-01 01:02:43 -0600

    accept mail

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   'sup'
+end
+
+post '/webhooks/email' do
+  request.body.rewind
+  json = JSON.parse(request.body.read)
+
+  logger.info "new mail: #{json}"
 end
