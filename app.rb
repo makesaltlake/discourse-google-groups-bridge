85066af0b995b6e5a5fff86cd5c064f73923c2d6
Alex Boyd <alex@opengroove.org>
2017-04-01 02:33:25 -0600

    parse mail content and strip reply context

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'json'
+require 'email_reply_trimmer'
+require 'base64'
+require 'mail'
 
 get '/' do
   'sup'
@@ -11,5 +14,22 @@ post '/webhooks/sparkpost' do
   request.body.rewind
   json = JSON.parse(request.body.read)
 
-  logger.info "new mail: #{json}"
+  json.each do |mail|
+    content = mail['msys']['relay_message']['content']['email_rfc822']
+    content = Base64.decode64(content) if mail['msys']['relay_message']['content']['email_rfc822_is_base64']
+
+    mail = Mail.new(content)
+    mail = mail.text_part if mail.multipart?
+
+    unless mail.content_type.to_s.include?('text/plain')
+      logger.info 'mail appears to be HTML only, bailing'
+      return
+    end
+
+    body = EmailReplyTrimmer.trim(mail.body.decoded)
+
+    logger.info "new mail: #{body}"
+  end
+
+  nil
 end
